e5133be512659bc149b242bcd4beb1d8b5a64f6b
PRANAV KUMAR <76862193+pranavkumar4975@users.noreply.github.com>
2023-08-17 19:17:07 +0530

    created weatherapp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
+import "./App.css";
+import TopButtons from "./Components/TopButtons";
+import Inputs from "./Components/Inputs";
+import TimeAndLocation from "./Components/TimeAndLocation";
+import TempAndDetails from "./Components/TempAndDetails";
+import Forecast from "./Components/Forecast";
+import getWeatherData from "./Services/WeatherService";
 
 function App() {
+  const fetchWeather = async () => {
+    const data = await getWeatherData("weather", { q: "london" });
+    console.log(data);
+  };
+
+  fetchWeather();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <TopButtons />
+      <Inputs />
+
+      <TimeAndLocation />
+      <TempAndDetails />
+      <Forecast title="hourly forecast" />
+      <Forecast title="daily forecast" />
     </div>
   );
 }
---
+++ src/Services/WeatherService.js
@@ -0,0 +1,13 @@
+const API_KEY = "e00c2bc3accfa027861bf929ee928627";
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+const getWeatherData = (infoType, searchParams) => {
+  const url = new URL(BASE_URL + "/" + infoType);
+  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+
+  return fetch(url)
+    .then((res) => res.json())
+    .then((data) => data);
+};
+
+export default getWeatherData;
